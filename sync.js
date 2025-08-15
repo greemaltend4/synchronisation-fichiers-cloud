84dc030a68e6483e80168e170bbde5529760b173
greemaltend4 <dengestabiy@hotmail.com>
2025-08-14 22:37:03 -0500

    Add comments to clarify the syncFiles function behavior

--- sync.js
+++ sync.js
@@ -3,6 +3,11 @@ const path = require('path');
 const { google } = require('googleapis');
 const dropboxV2Api = require('dropbox-v2-api');
 
+/**
+ * Synchronizes files from a local path to a specified cloud service.
+ * @param {string} localPath - The path to the local files to be synced.
+ * @param {string} cloudService - The name of the cloud service to sync to.
+ */
 function syncFiles(localPath, cloudService) {
     // Logique de synchronisation ici en fonction du service cloud choisi
     console.log(`Synchronisation de fichiers à partir de ${localPath} vers ${cloudService}`);
