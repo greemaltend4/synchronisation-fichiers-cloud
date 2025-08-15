bbda5a3dc31a023e4590ce6c5aaa22679fbb5d3a
greemaltend4 <dengestabiy@hotmail.com>
2025-08-14 22:24:49 -0500

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    google: {
+        clientId: 'YOUR_GOOGLE_CLIENT_ID',
+        clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+        redirectUri: 'http://localhost:3000/auth/google/callback'
+    },
+    dropbox: {
+        appKey: 'YOUR_DROPBOX_APP_KEY'
+    }
+};
